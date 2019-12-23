9bb0bf5fd91b528c186630e331c8f63c862df48d
Adrian Wong <adr.wong@mail.utoronto.ca>
2019-12-23 04:42:18 -0500

    nav now prevents scroll on mobile, added proj init pages, social links on contact

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,13 @@
     // HEADER
     $(".navbar").headroom();
 
+	// Prevent mobile nav scrollingss
+	$('.navbar-collapse').on('shown.bs.collapse', function() { 
+	   $('body').css('overflow', 'hidden');
+	});
+		
+	$('.navbar-collapse').on('hidden.bs.collapse', function() {
+	   $('body').css('overflow', 'auto');
+	});
+
 })(jQuery);
