f840176eaf63204bcf1aa446998f82c09fb639e8
Adrian Wong <adr.wong@mail.utoronto.ca>
2020-01-11 16:03:52 -0500

    fixed mobile bugs and nav bug?

--- js/custom.js
+++ js/custom.js
@@ -1,41 +1,39 @@
 (function ($) {
 
-  "use strict";
-   	$(document).ready(function() {
-	    // HEADER
-	    $(".navbar").headroom();
+	"use strict";
+	// HEADER
+	$(".navbar").headroom();
 
-		// Prevent mobile nav scrollingss
-		$('.navbar-collapse').on('shown.bs.collapse', function() { 
-		   $('body').css('overflow', 'hidden');
-		});
-			
-		$('.navbar-collapse').on('hidden.bs.collapse', function() {
-		   $('body').css('overflow', 'auto');
-		});
-
-		$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-	        event.preventDefault();
-	        $(this).ekkoLightbox({maxWidth: 1500});
-	    });
-
-	    const $dropdown = $(".dropdown");
-		const $dropdownToggle = $(".dropdown-toggle");
-		const $dropdownMenu = $(".dropdown-menu");
-		const showClass = "show";
-	    $dropdown.hover(
-	      function() {
-	        const $this = $(this);
-	        $this.addClass(showClass);
-	        $this.find($dropdownToggle).attr("aria-expanded", "true");
-	        $this.find($dropdownMenu).addClass(showClass);
-	      },
-	      function() {
-	        const $this = $(this);
-	        $this.removeClass(showClass);
-	        $this.find($dropdownToggle).attr("aria-expanded", "false");
-	        $this.find($dropdownMenu).removeClass(showClass);
-	      }
-	    );
+	// Prevent mobile nav scrollingss
+	$('.navbar-collapse').on('shown.bs.collapse', function() { 
+	   $('body').css('overflow', 'hidden');
 	});
+		
+	$('.navbar-collapse').on('hidden.bs.collapse', function() {
+	   $('body').css('overflow', 'auto');
+	});
+
+	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+	    event.preventDefault();
+	    $(this).ekkoLightbox({maxWidth: 1500});
+	});
+
+	const $dropdown = $(".dropdown");
+	const $dropdownToggle = $(".dropdown-toggle");
+	const $dropdownMenu = $(".dropdown-menu");
+	const showClass = "show";
+	$dropdown.hover(
+	  function() {
+	    const $this = $(this);
+	    $this.addClass(showClass);
+	    $this.find($dropdownToggle).attr("aria-expanded", "true");
+	    $this.find($dropdownMenu).addClass(showClass);
+	  },
+	  function() {
+	    const $this = $(this);
+	    $this.removeClass(showClass);
+	    $this.find($dropdownToggle).attr("aria-expanded", "false");
+	    $this.find($dropdownMenu).removeClass(showClass);
+	  }
+	);
 })(jQuery);
