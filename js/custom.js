ed641dfc3382f8ed1899eef1b8ded866ed9bfd1b
Adrian Wong <adr.wong@mail.utoronto.ca>
2020-01-11 03:34:24 -0500

    redid resume

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,41 @@
 (function ($) {
 
   "use strict";
-  
-    // HEADER
-    $(".navbar").headroom();
+   	$(document).ready(function() {
+	    // HEADER
+	    $(".navbar").headroom();
 
-	// Prevent mobile nav scrollingss
-	$('.navbar-collapse').on('shown.bs.collapse', function() { 
-	   $('body').css('overflow', 'hidden');
+		// Prevent mobile nav scrollingss
+		$('.navbar-collapse').on('shown.bs.collapse', function() { 
+		   $('body').css('overflow', 'hidden');
+		});
+			
+		$('.navbar-collapse').on('hidden.bs.collapse', function() {
+		   $('body').css('overflow', 'auto');
+		});
+
+		$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+	        event.preventDefault();
+	        $(this).ekkoLightbox({maxWidth: 1500});
+	    });
+
+	    const $dropdown = $(".dropdown");
+		const $dropdownToggle = $(".dropdown-toggle");
+		const $dropdownMenu = $(".dropdown-menu");
+		const showClass = "show";
+	    $dropdown.hover(
+	      function() {
+	        const $this = $(this);
+	        $this.addClass(showClass);
+	        $this.find($dropdownToggle).attr("aria-expanded", "true");
+	        $this.find($dropdownMenu).addClass(showClass);
+	      },
+	      function() {
+	        const $this = $(this);
+	        $this.removeClass(showClass);
+	        $this.find($dropdownToggle).attr("aria-expanded", "false");
+	        $this.find($dropdownMenu).removeClass(showClass);
+	      }
+	    );
 	});
-		
-	$('.navbar-collapse').on('hidden.bs.collapse', function() {
-	   $('body').css('overflow', 'auto');
-	});
-
-	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox({maxWidth: 1500});
-    });
-
-    const $dropdown = $(".dropdown");
-	const $dropdownToggle = $(".dropdown-toggle");
-	const $dropdownMenu = $(".dropdown-menu");
-	const showClass = "show";
-    $dropdown.hover(
-      function() {
-        const $this = $(this);
-        $this.addClass(showClass);
-        $this.find($dropdownToggle).attr("aria-expanded", "true");
-        $this.find($dropdownMenu).addClass(showClass);
-      },
-      function() {
-        const $this = $(this);
-        $this.removeClass(showClass);
-        $this.find($dropdownToggle).attr("aria-expanded", "false");
-        $this.find($dropdownMenu).removeClass(showClass);
-      }
-    );
 })(jQuery);
