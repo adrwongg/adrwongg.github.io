7c31a1d91bcbf0d480142c1d16c656bfed84abb1
Adrian Wong <adr.wong@mail.utoronto.ca>
2020-01-11 18:15:38 -0500

    hopefully fixed js load time

--- js/custom.js
+++ js/custom.js
@@ -3,21 +3,6 @@
 	"use strict";
 	// HEADER
 	$(".navbar").headroom();
-
-	// Prevent mobile nav scrollingss
-	$('.navbar-collapse').on('shown.bs.collapse', function() { 
-	   $('body').css('overflow', 'hidden');
-	});
-		
-	$('.navbar-collapse').on('hidden.bs.collapse', function() {
-	   $('body').css('overflow', 'auto');
-	});
-
-	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-	    event.preventDefault();
-	    $(this).ekkoLightbox({maxWidth: 1500});
-	});
-
 	const $dropdown = $(".dropdown");
 	const $dropdownToggle = $(".dropdown-toggle");
 	const $dropdownMenu = $(".dropdown-menu");
@@ -36,4 +21,18 @@
 	    $this.find($dropdownMenu).removeClass(showClass);
 	  }
 	);
+
+	// Prevent mobile nav scrollingss
+	$('.navbar-collapse').on('shown.bs.collapse', function() { 
+	   $('body').css('overflow', 'hidden');
+	});
+		
+	$('.navbar-collapse').on('hidden.bs.collapse', function() {
+	   $('body').css('overflow', 'auto');
+	});
+
+	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+	    event.preventDefault();
+	    $(this).ekkoLightbox({maxWidth: 1500});
+	});
 })(jQuery);
